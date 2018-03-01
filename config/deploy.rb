14120eeaa6d09a3b718b1db4be4255b200b04944
gary <yiso05255@gmail.com>
2018-03-01 16:14:08 +0800

    change git repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "qtreportmysql-master"
-set :repo_url, "git@github.com:YisoTsao/qtreportmysql.git"
+set :application, "qtreportmysql-master" #"quallist"
+set :repo_url, "git@github.com:YisoTsao/garyhome.git"
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
