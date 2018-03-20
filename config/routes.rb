427e07d0d08c991952aa6f1f56592a8de1f03d27
gary <yiso05255@gmail.com>
2018-03-20 22:48:19 +0800

    edit home page change to posts

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ resources :eflashssds do
     collection { post :import }
 end
 
-  root to: 'pages#home'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
